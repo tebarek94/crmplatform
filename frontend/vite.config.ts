36e690b094498ae8b43beba2dae730345d7bc3a1
tebarek94 <tebarekabas4@gmail.com>
2025-10-25 00:35:54 +0300

    add

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,12 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build'
+  },
   server: {
     port: 5173,
     host: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://crmplatform-tsv2.onrender.com',
         changeOrigin: true,
         secure: false,
       }
